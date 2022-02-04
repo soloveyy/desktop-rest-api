1c527f6ef44da78be8287af68ba5ec577f3ae7f1
soloveyy <solovyov.dd@gmail.com>
2022-02-04 23:03:24 +0100

    Create desktop-rest-api app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=>{
+      const url =`https://fnp5vd20r2.execute-api.us-east-1.amazonaws.com/dev/directories/3`
+      const fetchData = async(res) => {
+      const response = await fetch(res)
+      const data = await response.json()
+      console.log('render');
+      setData(data)
+      setIsLoading(false)
+      console.log(data.files);
+      const map = data.files.map(file=>console.log(file.name))
+    }
+    fetchData(url)
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {isLoading ? <h1>Loading...</h1> : <div><p>{data.name}</p><p>
+            {data.files.map(file=><p key={file.name}>{file.name}</p>)}
+            </p></div>}
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
